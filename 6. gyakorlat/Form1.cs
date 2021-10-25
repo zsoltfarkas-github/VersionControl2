b57eb8da1f27a95a8a9d5875fafc6fd07d277be3
Zsolt Márk Farkas <zsolt.farkas2@stud.uni-corvinus.hu>
2021-10-25 23:24:47 +0200

    3. feladatresz

--- 6. gyakorlat/Form1.cs
+++ 6. gyakorlat/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _6.gyakorlat.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,25 @@ namespace _6.gyakorlat
         public Form1()
         {
             InitializeComponent();
+
+            meghivas();
         }
+
+        private void meghivas()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+        
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+        }
+
     }
 }
